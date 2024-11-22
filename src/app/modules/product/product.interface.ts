f01c2efb2cb562490f677b55bddc44a55c7b0ff6
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-22 09:15:08 +0600

    feat: add TProduct type definition
    - Defined TProduct with fields: name, brand, price, category, description, quantity, and inStock

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,19 @@
+export type TProduct = {
+  name: string;
+  brand: string;
+  price: number;
+  category: string;
+  description: string;
+  quantity: number;
+  inStock: boolean;
+};
+
+// {
+//     "name": "Notebook",
+//     "brand": "Moleskine",
+//     "price": 15,
+//     "category": "Office Supplies",
+//     "description": "A high-quality notebook for professionals.",
+//     "quantity": 200,
+//     "inStock": true
+//   }
