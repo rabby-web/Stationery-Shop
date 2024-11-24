836d08402ee9ca53d20483e47000b525f0dbf5eb
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-24 09:20:46 +0600

    fix: server file import

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
 
 async function main() {
   try {
